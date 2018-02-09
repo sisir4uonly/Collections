802eaf7d6fabfdac6f3981983cedbc3d9391177c
Sisir <sisir4uonly@gmail.com>
2018-02-09 16:06:08 -0500

    2nd correct

--- src/com/collections/ProductCatalogue.java
+++ src/com/collections/ProductCatalogue.java
@@ -1,13 +1,12 @@
 package com.collections;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Set;
 
 public class ProductCatalogue implements Iterable<Product> {
 
-	private List<Product> products = new ArrayList<>();
-	// private Set<Product> products = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 
 	public void isSuppliedBy(Supplier supplier) {
 
